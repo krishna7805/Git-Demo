60e6abc225cc6ae9ad7ac26a44ac3af50775f418
Krishna <bardemohan08@gmail.com>
2025-03-21 16:33:30 +0530

    Second

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ class Hello{
     public static void main(String[] args)
     {
         System.out.println("Hello ton me");
+        int a = 10, b = 10;
+        System.out.println(a + b);
     }
 }
